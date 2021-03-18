47b0b35020861ba4a69435772b5f51535af97676
Sergio <sbouden16@gmail.com>
2021-03-18 09:54:43 +0100

    Add HTML form, params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_name
+
   def new; end
 
-  def create; end
+  def create
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_name
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :id)
+  end
 end
